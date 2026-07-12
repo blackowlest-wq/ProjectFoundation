9f91602cb68f87d832a8255c101c49215a8f7c18
blackowly <yosiki_85@hotmail.co.jp>
2026-07-13 06:49:02 +0900

    test: restore daily report separation guard

--- backend/src/test/java/com/example/dailyreport/report/DailyReportSeparationTest.java
+++ backend/src/test/java/com/example/dailyreport/report/DailyReportSeparationTest.java
@@ -4,39 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import com.example.dailyreport.auth.ManagerGroupPermissionRepository;
-import com.example.dailyreport.master.MasterDataRepository;
-import com.example.dailyreport.report.controller.DailyReportController;
-import com.example.dailyreport.report.entity.DailyReportRepository;
 
-@SpringBootTest(classes = DailyReportSeparationTest.TestApplication.class, properties = {
-        "spring.autoconfigure.exclude=" +
-                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration," +
-                "org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration"
-})
+@SpringBootTest
 @ActiveProfiles("test")
 class DailyReportSeparationTest {
     @Autowired
     ApplicationContext applicationContext;
-
-    @MockBean
-    DailyReportRepository dailyReportRepository;
-
-    @MockBean
-    MasterDataRepository masterDataRepository;
-
-    @MockBean
-    ManagerGroupPermissionRepository managerGroupPermissionRepository;
 
     @Test
     void separatedUseCaseBeansReplaceMixedDailyReportBeans() {
@@ -49,11 +25,5 @@ class DailyReportSeparationTest {
         assertThat(applicationContext.containsBean("dailyReportAccessPolicy")).isTrue();
         assertThat(applicationContext.containsBean("dailyReportController")).isFalse();
         assertThat(applicationContext.containsBean("dailyReportService")).isFalse();
-    }
-
-    @SpringBootConfiguration
-    @EnableAutoConfiguration
-    @Import({DailyReportController.class, DailyReportService.class})
-    static class TestApplication {
     }
 }
